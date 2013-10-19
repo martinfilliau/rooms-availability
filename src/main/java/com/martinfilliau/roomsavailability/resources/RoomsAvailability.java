8079df1e7dfcf282f9dfc10f5cc257bbc330ad9b
Martin Filliau <martin@filliau.com>
2013-10-19 18:42:10 +0100

    Handle date

--- src/main/java/com/martinfilliau/roomsavailability/resources/RoomsAvailability.java
+++ src/main/java/com/martinfilliau/roomsavailability/resources/RoomsAvailability.java
@@ -3,13 +3,15 @@ package com.martinfilliau.roomsavailability.resources;
 import com.martinfilliau.roomsavailability.configuration.ExchangeConfiguration;
 import com.martinfilliau.roomsavailability.representations.BusyPeriods;
 import com.martinfilliau.roomsavailability.services.ExchangeService;
-import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -33,15 +35,22 @@ public class RoomsAvailability {
     }
     
     @GET
-    public BusyPeriods busy(@QueryParam("email") String email) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+    public BusyPeriods busy(@QueryParam("email") String email,
+                            @QueryParam("date") @DefaultValue("today") String date) {
         Date start;
         Date end;
-        try {
-            start = sdf.parse("2013/10/18");
-            end = sdf.parse("2013/10/25");
-        } catch (ParseException ex) {
-            throw new WebApplicationException(ex, 400);
+        if("today".equals(date)) {
+            start = getTodayWithHour(0, 0);
+            end = getNextDay(start);
+        } else {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");            
+            try {
+                Date day = sdf.parse(date);
+                start = getDateWithHour(day, 0, 0);
+                end = getNextDay(start);
+            } catch (ParseException ex) {
+                throw new WebApplicationException(ex, 400);
+            }
         }
         try {
             return this.service.findBusyPeriods(email, start, end);
@@ -51,4 +60,26 @@ public class RoomsAvailability {
         }
     }
     
+    private Date getDateWithHour(Date date, int hour, int minutes) {
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        cal.set(Calendar.HOUR_OF_DAY, hour);
+        cal.set(Calendar.MINUTE, minutes);
+        return cal.getTime();
+    }
+    
+    private Date getTodayWithHour(int hour, int minutes) {
+        Calendar cal = new GregorianCalendar();
+        cal.set(Calendar.HOUR_OF_DAY, hour);
+        cal.set(Calendar.MINUTE, minutes);
+        return cal.getTime();
+    }
+
+    private Date getNextDay(Date date) {
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        int current = cal.get(Calendar.DAY_OF_YEAR);
+        cal.set(Calendar.DAY_OF_YEAR, current + 1);
+        return cal.getTime();
+    }
 }
